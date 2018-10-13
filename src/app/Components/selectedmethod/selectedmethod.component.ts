d8940267fb0127d06a997e85f5dc1bbe10fea7c5
Niladri28dutta <nild2141@gmail.com>
2018-10-13 14:51:48 +0530

    add arabic and german translation

--- src/app/Components/selectedmethod/selectedmethod.component.ts
+++ src/app/Components/selectedmethod/selectedmethod.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit,Input } from '@angular/core';
+import {arrayMethodOptions} from '../../languages/defaultconfig'
 
 @Component({
   selector: 'app-selectedmethod',
@@ -9,10 +10,28 @@ export class SelectedmethodComponent implements OnInit {
 
   @Input() methodType:string = '';
   @Input() methodFindOption:string = '';
+  selectedMethod:string = '';
+  options:string[] = [];
   
-  constructor() { }
+  constructor() { 
+   
+    
+  }
 
   ngOnInit() {
+    let selectedOption = '';
+    let selectedFindOptions = [];
+    if(this.methodFindOption === ''){
+      console.log(this.methodType);
+      selectedOption = Object.keys(arrayMethodOptions.status).filter( x => x === this.methodType)[0];
+      this.options = arrayMethodOptions.status[selectedOption];
+    }
+    else{
+      this.options = Object.keys(arrayMethodOptions.status).filter( x => x === this.methodType)
+                                                           .map( val => {return arrayMethodOptions.status[val]} )
+                                                           .filter( fn => fn === this.methodFindOption);
+    } 
+    console.log(this.options);
   }
 
 }
