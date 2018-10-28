eb066fd2195d47c2481a25753a93b32ddb43bd4b
Niladri Dutta <nild2141@gmail.com>
2018-10-28 16:28:23 +0000

    Add electron build

---
+++ main.js
@@ -0,0 +1,48 @@
+const {app, BrowserWindow} = require('electron')
+const path = require('path')
+const url = require('url')
+
+let win
+
+function createWindow () {
+  win = new BrowserWindow(
+      {width: 800, 
+      height: 600,
+      backgroundColor: '#ffffff',
+      icon: url.format({
+        pathname: path.join(__dirname, 'dist/assets/icon/angular-electron.ico'),
+        protocol: 'file:',
+        slashes: true
+      }),
+      autoHideMenuBar:true
+   })
+
+  // load the dist folder from Angular
+  win.loadURL(url.format({
+    pathname: path.join(__dirname, 'dist/index.html'),
+    protocol: 'file:',
+    slashes: true
+  }))
+
+  // Open the DevTools optionally:
+  // win.webContents.openDevTools()
+
+  win.on('closed', () => {
+    win = null
+  })
+}
+
+app.on('ready', createWindow)
+
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit()
+  }
+})
+
+app.on('activate', () => {
+  if (win === null) {
+    createWindow()
+  }
+})
